e0be6fe8a1056fc883ef8daf0f06753ea50fa872
Muhammad Umair <mumair989@gmail.com>
2023-10-10 16:51:39 -0400

    added a new json

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
-const petPromise = await fetch("https://learnwebcode.github.io/pet-adoption-data/pets.json")
+const petPromise = await fetch("https://github.com/mumair989/355-Project1/animals.json")
 const pets = await petPromise.json()
 
 const template = document.querySelector("#animal-card")
 const wrapper = document.createElement("div")
 pets.forEach(pet => {
     const clone = template.content.cloneNode(true)
+    clone.querySelector("h3").textContent = pet.name
+    
+    const img = clone.querySelector("img")
+    img.src = pet.photo
+    img.alt = `A ${pet.species} named ${pet.name}`
 
     wrapper.appendChild(clone)
 
