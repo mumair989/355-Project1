e558913ff68d12e93c43f3c3059af13cb26448de
Muhammad Umair <mumair989@gmail.com>
2023-10-15 21:43:32 -0400

    added a checkout page

--- submit.js
+++ submit.js
@@ -1,55 +1,4 @@
-// Get form data
-// const form = document.querySelector('form');
-// const formData = new FormData(form);
-
-// // Map form data to JSON object
-// const pet = {
-//   id: Date.now(),
-//   name: formData.get('name'),
-//   email: formData.get('email'),
-//   phone: formData.get('phone'),
-//   pet_name: formData.get('pet_name'),
-//   species: formData.get('species'),
-//   description: formData.get('description'),
-//   photo: formData.get('photo')
-// };
-
-// // Fetch JSON file and append new pet object
-// fetch('pets.json')
-//   .then(response => response.json())
-//   .then(data => {
-//     const jsonData = data || []; // Initialize with an empty array if 'pets.json' is empty or doesn't exist
-//     jsonData.push(pet);
-
-//     // Stringify array back to JSON
-//     const updatedJson = JSON.stringify(jsonData);
-
-//     // Save updated JSON data back to 'pets.json'
-//     fetch('animals.json', {
-//       method: 'PUT', // This will not work without a server-side handler
-//       body: updatedJson
-//     })
-//       .then(() => {
-//         // Redirect to index.html
-//         window.location.href = 'index.html';
-//       })
-//       .catch(error => {
-//         console.error('Error saving data:', error);
-//         // Handle the error gracefully
-//       });
-//   })
-//   .catch(error => {
-//     console.error('Error fetching data:', error);
-//     // Handle the error gracefully
-//   });
-
-
-
-
-
-
-
-  const http = require('http');
+const http = require('http');
 const fs = require('fs');
 const url = require('url');
 const qs = require('querystring');
@@ -105,3 +54,51 @@ const port = 3000;
 server.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+
+// Get form data
+// const form = document.querySelector('form');
+// const formData = new FormData(form);
+
+// // Map form data to JSON object
+// const pet = {
+//   id: Date.now(),
+//   name: formData.get('name'),
+//   email: formData.get('email'),
+//   phone: formData.get('phone'),
+//   pet_name: formData.get('pet_name'),
+//   species: formData.get('species'),
+//   description: formData.get('description'),
+//   photo: formData.get('photo')
+// };
+
+// // Fetch JSON file and append new pet object
+// fetch('pets.json')
+//   .then(response => response.json())
+//   .then(data => {
+//     const jsonData = data || []; // Initialize with an empty array if 'pets.json' is empty or doesn't exist
+//     jsonData.push(pet);
+
+//     // Stringify array back to JSON
+//     const updatedJson = JSON.stringify(jsonData);
+
+//     // Save updated JSON data back to 'pets.json'
+//     fetch('animals.json', {
+//       method: 'PUT', // This will not work without a server-side handler
+//       body: updatedJson
+//     })
+//       .then(() => {
+//         // Redirect to index.html
+//         window.location.href = 'index.html';
+//       })
+//       .catch(error => {
+//         console.error('Error saving data:', error);
+//         // Handle the error gracefully
+//       });
+//   })
+//   .catch(error => {
+//     console.error('Error fetching data:', error);
+//     // Handle the error gracefully
+//   });
+
+
